9ef38df6fcb0af4147de756024fc614322b72f9a
coronies <podkovyrov.tymur@gmail.com>
2024-10-26 10:55:14 -0500

    updare for the startin

--- connecting-website/src/App.js
+++ connecting-website/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          hi everyone ! - via
+          hi everyone ! - tim
         </p>
         <a
           className="App-link"
