04b2d1d8823b4fbc8c1d6f103448c1fe6b11911f
Via <viaguasa@utexas.edu>
2024-10-30 18:24:54 -0500

    homepage components

--- connecting-website/src/App.js
+++ connecting-website/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import BasicInfo from './components/BasicInfo';
+import Overview from './components/Overview';
+import Events from './components/Events';
+import FAQ from './components/FAQ';
+import Demographics from './components/Demographics';
+import Officers from './components/Officers';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          hi everyone ! - tim
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          edtech connecting!
-        </a>
+      <Navbar />
+      <header className="header">
+        <h1>Organization Name</h1>
+        <div className="tags">
+          <span className="tag">Tag Info</span>
+          <span className="tag">Tag Info</span>
+          <span className="tag">Tag Info</span>
+        </div>
       </header>
+      <div className="content">
+        <BasicInfo />
+        <Overview />
+        <Events />
+        <FAQ />
+        <Demographics />
+        <Officers />
+      </div>
     </div>
   );
 }
