733d40abc7ae422af0cb475778cbc742b2ae7909
Via <viaguasa@utexas.edu>
2024-11-18 20:54:13 -0600

    routed quiz, clubprofile, best match, and clubPOV

--- connecting-website/src/App.js
+++ connecting-website/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ClubProfile from './clubprofile/ClubProfile';
 import SearchPage from './bestmatch/SearchPage';
 import ClubPOV from './clubpov/ClubPOV';
+import Quiz from './quiz/Quiz';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<ClubProfile />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/clubpov" element={<ClubPOV />} />
+        <Route path="/quiz" element={<Quiz />}/>
       </Routes>
     </Router>
   );
